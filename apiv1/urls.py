a126949d92e8aa4ae4f7d578febfc2ecc6ee835e
yomi <abayomi4mercy@yahoo.com>
2023-01-13 13:25:59 +0100

    first commit

---
+++ apiv1/urls.py
@@ -0,0 +1,14 @@
+from rest_framework.authtoken.views import obtain_auth_token
+from django.urls import path, include
+from . import views
+
+
+app_name = "apiv1"
+
+
+urlpatterns = [
+    path("auth/", obtain_auth_token, name="obtain_auth_token"),
+    path("account/", include("account.urls_v1", namespace="account_url")),
+    path("core/", include("core.urls_v1", namespace="core_url")),
+    path("", views.home, name="home"),
+]
